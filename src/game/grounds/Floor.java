3725940846daa315f91ff6b921d3f0bbaae87ba9
HUANGGUOYUEYANG <ghua0010@student.monash.edu>
2022-04-22 23:56:09 +0800

    Completed REQ3 Update: Enemies

--- src/game/grounds/Floor.java
+++ src/game/grounds/Floor.java
@@ -1,12 +1,26 @@
 package game.grounds;
 
+import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
+import game.Status;
 
 /**
  * A class that represents the floor inside a building.
  */
 public class Floor extends Ground {
-	public Floor() {
-		super('_');
-	}
+    public Floor() {
+        super('_');
+    }
+    private boolean canEnter = false;
+
+    @Override
+    public boolean canActorEnter(Actor actor) {
+        if (actor.hasCapability(Status.HOSTILE_TO_ENEMY)){
+            canEnter = true;
+        }
+        else if (actor.hasCapability(Status.IS_ENEMY)){
+            canEnter = false;
+        }
+        return canEnter;
+    }
 }
