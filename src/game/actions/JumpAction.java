16c782f760eda829a7e43b50421ed202fa7779f2
Kuah Jia Chen <andy.kuah1933@gmail.com>
2022-04-29 20:54:45 +0800

    Added javadocs for JumpAction and ResetAction class

--- src/game/actions/JumpAction.java
+++ src/game/actions/JumpAction.java
@@ -9,7 +9,14 @@ import game.grounds.HighGround;
 
 import java.util.Random;
 
+/**
+ * JumpAction is a class that allows the actor to jump to the jumpable ground.
+ * It is a new class that extends Action class. When there is a high ground nearby
+ * player (i.e., one of the eight exits) , the jump action will be displayed on the
+ * console and allow the player to jump.
+ */
 public class JumpAction extends Action {
+
     /**
      * Target location
      */
@@ -19,16 +26,38 @@ public class JumpAction extends Action {
      */
     private String direction;
 
+    /**
+     * Random number generator
+     */
     private final Random rand = new Random();
 
+    /**
+     * Target high ground
+     */
     private HighGround highGround;
 
+    /**
+     * Constructor of JumpAction class
+     * @param moveToLocation Target location
+     * @param direction the direction of the move (to be displayed in menu)
+     * @param highGround the target high ground
+     */
     public JumpAction(Location moveToLocation, String direction, HighGround highGround) {
         this.moveToLocation = moveToLocation;
         this.direction = direction;
         this.highGround = highGround;
     }
 
+    /**
+     * When the jump action is selected by the user, this execute method will be called.
+     * It will first check if the actor consumed super mushrooms or not. If the actor does
+     * not consume a super mushroom and the random number generated is greater than the
+     * success rate, the actor will fail to jump to the target high ground. Otherwise, the
+     * jump action will be successful. The corresponding message also will be return
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return a description of the jump action to inform the user whether a success or fail jump is performed
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         // TODO: create variables to hold the values for each return of methods
@@ -49,6 +78,12 @@ public class JumpAction extends Action {
         }
     }
 
+    /**
+     * Return a description of this jump action to display in the menu.
+     * Besides that, the coordinate of the target high ground also will be printed out.
+     * @param actor The actor performing the action.
+     * @return a String that describe the current jump action
+     */
     @Override
     public String menuDescription(Actor actor) {
         return actor + " jumps to the " + highGround.getGroundType() + "(" +moveToLocation.x() + ", "
--- src/game/actions/ResetAction.java
+++ src/game/actions/ResetAction.java
@@ -5,18 +5,42 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.managers.ResetManager;
 
+/**
+ * ResetAction is a class that allows the user to reset the game. It is a class
+ * that will reset the game when the user selects the reset option on the menu.
+ * It is a new class that extends Action class.
+ */
 public class ResetAction extends Action {
+
+    /**
+     * When the reset action is selected by the user, this execute method will be called.
+     * It will get the instance of ResetManager and execute the run method in the
+     * ResetManager class. Eventually, a message will be printed out to inform the user
+     * the game is resetted.
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return a String that inform the user the game has been resetted
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         ResetManager.getInstance().run();
         return "The game is now reset";
     }
 
+    /**
+     * Return a description of this reset action to display in the menu.
+     * @param actor The actor performing the action.
+     * @return a String that describe the reset action
+     */
     @Override
     public String menuDescription(Actor actor) {
         return "Reset the game";
     }
 
+    /**
+     * Return's the hotkey of reset action
+     * @return a String that indicating the hotkey of reset action
+     */
     @Override
     public String hotkey() {
         return "r";
