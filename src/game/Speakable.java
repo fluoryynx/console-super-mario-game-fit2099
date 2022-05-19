281d289d475cefa745ee681f0994da20ac02b4ce
HUANGGUOYUEYANG <ghua0010@student.monash.edu>
2022-05-20 02:48:15 +0800

    key, monologue, speakable, status  documentation update

--- src/game/Speakable.java
+++ src/game/Speakable.java
@@ -1,12 +1,28 @@
 package game;
 
+/**
+ * It is an interface that will be implemented by those class that need to speak every even turn.
+ * The reason to create this interface instead of just directly putting the following two methods in
+ * each actor class is because this kind of design allow for extension.
+ *
+ * @author Huang GuoYueYang
+ */
 public interface Speakable {
 
+    /**
+     * A default interface method that generate the monologue sentence from Monologue instance by using boundary index
+     * @param monologueIndexLowerBound Lower boundary of index
+     * @param monologueIndexUpperBound Upper boundary of index
+     */
     default String generateMonologue(int monologueIndexLowerBound, int monologueIndexUpperBound){
         return Monologue.getInstance().getMonologue(monologueIndexLowerBound,monologueIndexUpperBound);
     }
 
+    /**
+     * A default interface method that check whether current turn is even turn or not
+     * @param currentTurn current turn of this game
+     */
     default boolean timeToSpeak(int currentTurn){
-        return currentTurn%2 == 0;
+        return currentTurn % 2 == 0;
     }
 }
