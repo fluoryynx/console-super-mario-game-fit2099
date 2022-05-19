7441945d88e40b109367aa71adbead76128226e4
HUANGGUOYUEYANG <ghua0010@student.monash.edu>
2022-05-20 02:23:02 +0800

    actors package documentation update

--- src/game/actors/enemies/koopas/FlyingKoopa.java
+++ src/game/actors/enemies/koopas/FlyingKoopa.java
@@ -1,8 +1,19 @@
 package game.actors.enemies.koopas;
 
 import game.Status;
-import game.actors.enemies.koopas.GeneralKoopa;
 
+/**
+ * FlyingKoopa class is a class represents one kind of GeneralKoopa of this game. It is a class that extends from the GeneralKoopa class.
+ * Flying Koopa F has pretty much similar characteristics as the original Koopa, except it has a bigger health bar (150 hit points).
+ * It starts with 30 damage and 50% hit rate.
+ * Furthermore, it can walk (fly) over the trees and walls when it wanders around (incl. other high grounds).
+ * It also applies when Flying Koopa follows Mario. Like Koopa, it will go to a dormant state whenever it is unconscious until
+ * Mario brings a wrench to crack the shell (killing it)! Cracking its shell drops a Super Mushroom.
+ * The mature tree has a 50:50 chance of spawning either normal Koopa or Flying Koopa (after a successful 15% spawn rate).
+ * Besides, FlyingKoopa also speak every even turn automatically.
+ *
+ * @author Huang GuoYueYang
+ */
 public class FlyingKoopa extends GeneralKoopa {
     /**
      * Name of FlyingKoopa
@@ -21,6 +32,7 @@ public class FlyingKoopa extends GeneralKoopa {
 
     /**
      * Constructor.
+     * The monologue belongs to FlyingKoopa will be print by using index.
      */
     public FlyingKoopa() {
         super(FLYINGKOOPA_NAME,FLYINGKOOPA_CHAR,HIT_POINT,14,16);
--- src/game/actors/enemies/koopas/GeneralKoopa.java
+++ src/game/actors/enemies/koopas/GeneralKoopa.java
@@ -11,39 +11,52 @@ import edu.monash.fit2099.engine.positions.Location;
 import game.Status;
 import game.actions.AttackShellAction;
 import game.actors.enemies.Enemy;
-
 import java.util.List;
 
+/**
+ * GeneralKoopa class is an abstract class represents all Koopa' base of this game. It is a class that extends from the Enemy class.
+ * GeneralKoopa will move around in the game map if no player is beside it but it cannot enter floor.
+ * Once GeneralKoopa is engaged in a fight (the Player attacks the enemy or the enemy attacks player
+ * -- when the player stands in the enemy's surroundings), it will follow the Player.
+ * When defeated(is not conscious), it will not be removed from the map. Instead, it will go to a dormant
+ * state (D) and stay on the ground (cannot attack nor move), all the behaviors will removed from GeneralKoopa(attack/follow/wander), its character will change from ’K’ to ‘D’.
+ * Mario needs a Wrench (80% hit rate and 50 damage), the only weapon to destroy Koopa's shell.
+ * Destroying its shell will drop a Super Mushroom.
+ * Different type of Koopa starts with different hit points.
+ * Besides, GeneralKoopa also speak every even turn automatically.
+ *
+ * @author Huang GuoYueYang
+ */
 public abstract class GeneralKoopa extends Enemy {
 
     /**
-     * State of Koopa
+     * State of GeneralKoopa
      */
     private boolean isDefeated = false;
 
     /**
-     * Character of Koopa's shell
+     * Character of GeneralKoopa's shell
      */
     private static final char SHELL_CHAR = 'D';
 
     /**
-     * HitRate of Koopa
+     * HitRate of GeneralKoopa
      */
     private static final int HIT_RATE = 50;
 
     /**
-     * Damage of Koopa
+     * Damage of GeneralKoopa
      */
     private static final int DAMAGE = 30;
 
     /**
-     * Hit verb of Koopa
+     * Hit verb of GeneralKoopa
      */
     private static final String HIT_VERB = "punch";
 
     /**
      * Constructor.
-     *
+     * The monologue belongs to GeneralKoopa will be print by using index.
      * @param name        the name of the Actor
      * @param displayChar the character that will represent the Actor in the display
      * @param hitPoints   the Actor's starting hit point
@@ -54,9 +67,9 @@ public abstract class GeneralKoopa extends Enemy {
     }
 
     /**
-     * Make Koopa can be attacked by Player.
-     * When Koopa is not conscious(defeated), it will hide in shell and only the actor with capability HOSTILE_TO_ENEMY && HAVE_WRENCH
-     * is allowed smash its shell.
+     * Make GeneralKoopa can be attacked by Player.
+     * When GeneralKoopa is not conscious(defeated), it will hide in shell and only the actor with capability HOSTILE_TO_ENEMY && HAVE_WRENCH
+     * is allowed to smash its shell in order to get a super mushroom.
      * @param otherActor the Actor that might perform an action.
      * @param direction  String representing the direction of the other Actor
      * @param map        current GameMap
@@ -84,7 +97,7 @@ public abstract class GeneralKoopa extends Enemy {
 
     /**
      * Select and return an action to perform on the current turn.
-     * If Koopa is not conscious anymore, it is defeated.
+     * If GeneralKoopa is not conscious anymore, it is defeated.
      * At this time, it will turn into shell states and all the behaviors will be removed.
      * @param actions    collection of possible Actions for this Actor
      * @param lastAction The Action this Actor took last turn. Can do interesting things in conjunction with Action.getNextAction()
