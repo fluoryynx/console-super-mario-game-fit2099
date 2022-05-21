763dbbae44736150f1de7fe82f9f77f236fb291a
Kuah Jia Chen <andy.kuah1933@gmail.com>
2022-05-21 15:51:16 +0800

    Updated FireFlower class

--- src/game/items/magicalitems/FireFlower.java
+++ src/game/items/magicalitems/FireFlower.java
@@ -7,10 +7,14 @@ import game.items.magicalitems.MagicalItem;
 
 public class FireFlower extends MagicalItem {
 
+    private static final String MAGICAL_ITEM_NAME = "Fire Flower";
+    private static final char MAGICAL_ITEM_CHAR = 'f';
+    private static final boolean MAGICAL_ITEM_PORTABLE = true;
+    private static final boolean MAGICAL_ITEM_DROPPABLE = false;
     private int remainingTurn = 20;
 
     public FireFlower() {
-        super("Fire Flower",'f',true,false);
+        super(MAGICAL_ITEM_NAME,MAGICAL_ITEM_CHAR,MAGICAL_ITEM_PORTABLE,MAGICAL_ITEM_DROPPABLE);
     }
 
     @Override
