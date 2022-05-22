68d2307b82a9eb2374bb1060bf7d0b4a313ab777
Kuah Jia Chen <andy.kuah1933@gmail.com>
2022-05-22 20:02:48 +0800

    Updated Javadoc for Status enum

--- src/game/Status.java
+++ src/game/Status.java
@@ -7,32 +7,143 @@ package game;
  * @author Huang GuoYueYang, Kuah Jia Chen, Lim Fluoryynx
  */
 public enum Status {
-    HOSTILE_TO_ENEMY, // use this status to be considered hostile towards enemy (e.g., to be attacked by enemy)
-    TALL, // use this status to tell that current instance has "grown".
-    CAN_JUMP, // use this status to indicate the actor can jump to a particular high ground
-    IS_FERTILE, // use this status to indicate that a particular ground is a fertile ground
-    HAVE_WRENCH, // use this status to indicate the player have wrench now
-    HIDE_IN_SHELL, // use this status to determine actor is a Koopa
-    IS_ENEMY, // use this status to determine all enemies
-    ATTACKED_BY_ENEMY, // use this status to indicate actor is attacked by enemy
-    INVINCIBLE, // use this status to determine player has consumed the Power star
-    CONSUMABLE, // use this status to indicate that the item can be consumed by player
-    BUY, // use this status to indicate that the actor is able to buy
-    SPEAK, // use this status to indicate that can speak to toad
-    RESET_CALLED, // use this status to indicate that player enters the reset hotkey
-    TELEPORT, // use this status to indicate an actor can teleport
-    FIRST_MAP, // use this status to indicate the actor is currently on the first map
-    SECOND_MAP, // use this status to indicate the actor is currently on the second map
-    HAVE_KEY, // use this status to indicate that player has key or not
-    DROP_KEY, // use this status to indicate that the actor has capability to drop the key or not
-    CAN_FLY, // use this status to indicate tha the actor has capability of fly or not
-    FIRE_ATTACK, // use this status to indicate the actor can use fire attack
-    FIRE_DAMAGE, // use this status to indicate a particular ground can do fire damage
-    HAS_BOTTLE, // use this status to indicate actor had a bottle in inventory
-    POWER_WATER, // use this status to indicate actor drank power water
-    IS_FOUNTAIN, // use this status to indicate the ground is a fountain
-    INCREASE_HEALTH, // use this status to indicate the ground is a health fountain
-    INCREASE_BASE_DAMAGE, //use this status to indicate the ground is a power fountain
-    DRANK_BY_ENEMY, // use this status to indicate water in the fountain is drank by enemies
-    IS_EMPTY // use this status to indicate fountain is empty
+    /**
+     * use this status to be considered hostile towards enemy (e.g., to be attacked by enemy)
+     */
+    HOSTILE_TO_ENEMY,
+
+    /**
+     * use this status to tell that current instance has "grown".
+     */
+    TALL,
+
+    /**
+     * use this status to indicate the actor can jump to a particular high ground
+     */
+    CAN_JUMP,
+
+    /**
+     * use this status to indicate that a particular ground is a fertile ground
+     */
+    IS_FERTILE,
+
+    /**
+     * use this status to indicate the player have wrench now
+     */
+    HAVE_WRENCH,
+
+    /**
+     * use this status to determine actor is a Koopa
+     */
+    HIDE_IN_SHELL,
+
+    /**
+     * use this status to determine all enemies
+     */
+    IS_ENEMY,
+
+    /**
+     * use this status to indicate actor is attacked by enemy
+     */
+    ATTACKED_BY_ENEMY,
+
+    /**
+     * use this status to determine player has consumed the Power star
+     */
+    INVINCIBLE,
+
+    /**
+     * use this status to indicate that the item can be consumed by player
+     */
+    CONSUMABLE,
+
+    /**
+     * use this status to indicate that the actor is able to buy
+     */
+    BUY,
+
+    /**
+     * use this status to indicate that can speak to toad
+     */
+    SPEAK,
+
+    /**
+     * use this status to indicate that player enters the reset hotkey
+     */
+    RESET_CALLED,
+
+    /**
+     * use this status to indicate an actor can teleport
+     */
+    TELEPORT,
+
+    /**
+     * use this status to indicate the actor is currently on the first map
+     */
+    FIRST_MAP,
+
+    /**
+     * use this status to indicate the actor is currently on the second map
+     */
+    SECOND_MAP,
+
+    /**
+     * use this status to indicate that player has key or not
+     */
+    HAVE_KEY,
+
+    /**
+     * use this status to indicate that the actor has capability to drop the key or not
+     */
+    DROP_KEY,
+
+    /**
+     * use this status to indicate tha the actor has capability of fly or not
+     */
+    CAN_FLY,
+
+    /**
+     * use this status to indicate the actor can use fire attack
+     */
+    FIRE_ATTACK,
+
+    /**
+     * use this status to indicate a particular ground can do fire damage
+     */
+    FIRE_DAMAGE,
+
+    /**
+     * use this status to indicate actor had a bottle in inventory
+     */
+    HAS_BOTTLE,
+
+    /**
+     * use this status to indicate actor drank power water
+     */
+    POWER_WATER,
+
+    /**
+     * use this status to indicate the ground is a fountain
+     */
+    IS_FOUNTAIN,
+
+    /**
+     * use this status to indicate the ground is a health fountain
+     */
+    INCREASE_HEALTH,
+
+    /**
+     * use this status to indicate the ground is a power fountain
+     */
+    INCREASE_BASE_DAMAGE,
+
+    /**
+     * use this status to indicate water in the fountain is drank by enemies
+     */
+    DRANK_BY_ENEMY,
+
+    /**
+     * use this status to indicate fountain is empty
+     */
+    IS_EMPTY
 }
